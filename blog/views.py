e84b5358ccc68f7b8dcc0ee53ee0c3d6bbb107e3
Micah Andres <limbo.linux.dev@gmail.com>
2020-04-04 18:57:14 -0700

    implemented django template inheritance and css @ 1857

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post comment',
+        'date_posted': 'April 4, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post comment',
+        'date_posted': 'April 5, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Cafeblog Home</h1>');
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Cafeblog About</h1>');
+    return render(request, 'blog/about.html', {'title': 'About'});
