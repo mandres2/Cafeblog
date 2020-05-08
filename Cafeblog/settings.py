5e65cf592990c2ea2caa908ef68abc174507e8eb
Micah Andres <limbo.linux.dev@gmail.com>
2020-05-07 19:05:56 -0700

    fix DisallowedHost at /

--- Cafeblog/settings.py
+++ Cafeblog/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://cafe-blog-1.herokuapp.com/']
+ALLOWED_HOSTS = ['cafe-blog-1.herokuapp.com']
 
 
 # Application definition
