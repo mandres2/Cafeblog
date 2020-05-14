ae5af1b16fbb8222dadd4b76f79a451d1f0380d4
Micah Andres <limbo.linux.dev@gmail.com>
2020-05-13 19:35:31 -0700

    updated fixing local deployment bug

--- Cafeblog/settings.py
+++ Cafeblog/settings.py
@@ -20,13 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'hnrcg=u@^()78xr2=ln_-i=0)$s+fv(&k&r%6j@k*jt&zewncn'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#TODO
+
+SECRET_KEY = ['hnrcg=u@^()78xr2=ln_-i=0)$s+fv(&k&r%6j@k*jt&zewncn', os.environ.get('SECRET_KEY')]
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cafe-blog-1.herokuapp.com']
+ALLOWED_HOSTS = ['cafe-blog-1.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
